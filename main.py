81e2a4aff8b19588bd16a0794141f4fa0670d13b
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-08-04 21:07:22 +0300

    Добавил типизацию через `enum`

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelLetter(str, Enum):
+    alpha = "alpha"
+    beta = "beta"
+    gamma = "gamma"
+
 
 app = FastAPI()
 
@@ -16,3 +25,14 @@ async def view():
 @app.get("/foo/{bar}")
 async def view(bar):
     return {"message": bar}
+
+
+@app.get("/letter/{model_letter}")
+async def view(model_letter: ModelLetter):
+    if model_letter is ModelLetter.alpha:
+        return {"model_letter": model_letter, "message": "Deep Learning FTW!"}
+
+    if model_letter.value == "beta":
+        return {"model_letter": model_letter, "message": "LeCNN all the images"}
+
+    return {"model_letter": model_letter, "message": "Have some residuals"}
