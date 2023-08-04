aae38b6b6df708b5ad655154baad3d9ed3adbc03
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-08-04 20:36:25 +0300

    Разобрался с параметрами в маршрутах

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.get("/foo/bar")
+async def view():
+    return {"message": "bar!"}
+
+
+@app.get("/foo/{bar}")
+async def view(bar):
+    return {"message": bar}
